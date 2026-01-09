2be90d8af69603479383beaaca2d344041ff643e
Lauracmd12 <lauracmd12@gmail.com>
2026-01-09 12:42:33 -0500

    Primer commit - proyecto inicial

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace CargarData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBuscarDocumento = new System.Windows.Forms.TextBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.lblResultadoConsulta = new System.Windows.Forms.Label();
+            this.dataClientes = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSeleccionarArchivo = new System.Windows.Forms.Button();
+            this.lblArchivoCargar = new System.Windows.Forms.Label();
+            this.btnCargarDatos = new System.Windows.Forms.Button();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.ListaMeses = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Consulta";
+            // 
+            // txtBuscarDocumento
+            // 
+            this.txtBuscarDocumento.Location = new System.Drawing.Point(20, 110);
+            this.txtBuscarDocumento.Name = "txtBuscarDocumento";
+            this.txtBuscarDocumento.Size = new System.Drawing.Size(166, 20);
+            this.txtBuscarDocumento.TabIndex = 0;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.BackColor = System.Drawing.Color.Aqua;
+            this.btnConsultar.Location = new System.Drawing.Point(254, 87);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(88, 43);
+            this.btnConsultar.TabIndex = 1;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = false;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // lblResultadoConsulta
+            // 
+            this.lblResultadoConsulta.AutoSize = true;
+            this.lblResultadoConsulta.Location = new System.Drawing.Point(14, 129);
+            this.lblResultadoConsulta.Name = "lblResultadoConsulta";
+            this.lblResultadoConsulta.Size = new System.Drawing.Size(0, 13);
+            this.lblResultadoConsulta.TabIndex = 2;
+            // 
+            // dataClientes
+            // 
+            this.dataClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataClientes.Location = new System.Drawing.Point(17, 144);
+            this.dataClientes.Name = "dataClientes";
+            this.dataClientes.Size = new System.Drawing.Size(1277, 307);
+            this.dataClientes.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 473);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cargar Informacion";
+            // 
+            // btnSeleccionarArchivo
+            // 
+            this.btnSeleccionarArchivo.Location = new System.Drawing.Point(12, 538);
+            this.btnSeleccionarArchivo.Name = "btnSeleccionarArchivo";
+            this.btnSeleccionarArchivo.Size = new System.Drawing.Size(75, 23);
+            this.btnSeleccionarArchivo.TabIndex = 5;
+            this.btnSeleccionarArchivo.Text = "Seleccionar";
+            this.btnSeleccionarArchivo.UseVisualStyleBackColor = true;
+            this.btnSeleccionarArchivo.Click += new System.EventHandler(this.btnSeleccionarArchivo_Click_1);
+            // 
+            // lblArchivoCargar
+            // 
+            this.lblArchivoCargar.AutoSize = true;
+            this.lblArchivoCargar.Location = new System.Drawing.Point(12, 522);
+            this.lblArchivoCargar.Name = "lblArchivoCargar";
+            this.lblArchivoCargar.Size = new System.Drawing.Size(49, 13);
+            this.lblArchivoCargar.TabIndex = 6;
+            this.lblArchivoCargar.Text = "Archivo: ";
+            // 
+            // btnCargarDatos
+            // 
+            this.btnCargarDatos.Location = new System.Drawing.Point(855, 512);
+            this.btnCargarDatos.Name = "btnCargarDatos";
+            this.btnCargarDatos.Size = new System.Drawing.Size(75, 23);
+            this.btnCargarDatos.TabIndex = 7;
+            this.btnCargarDatos.Text = "CARGAR";
+            this.btnCargarDatos.UseVisualStyleBackColor = true;
+            this.btnCargarDatos.Visible = false;
+            this.btnCargarDatos.Click += new System.EventHandler(this.btnCargarDatos_Click);
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado.ForeColor = System.Drawing.Color.Green;
+            this.lblEstado.Location = new System.Drawing.Point(14, 588);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(52, 17);
+            this.lblEstado.TabIndex = 8;
+            this.lblEstado.Text = "label3";
+            this.lblEstado.Visible = false;
+            // 
+            // ListaMeses
+            // 
+            this.ListaMeses.FormattingEnabled = true;
+            this.ListaMeses.Items.AddRange(new object[] {
+            "ENERO",
+            "FEBRERO",
+            "MARZO",
+            "ABRIL",
+            "MAYO",
+            "JUNIO",
+            "JULIO",
+            "AGOSTO",
+            "SEPTIEMBRE",
+            "OCTUBRE",
+            "NOVIEMBRE",
+            "DICIEMBRE"});
+            this.ListaMeses.Location = new System.Drawing.Point(531, 510);
+            this.ListaMeses.Name = "ListaMeses";
+            this.ListaMeses.Size = new System.Drawing.Size(120, 95);
+            this.ListaMeses.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(855, 588);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "DESCARGAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1306, 657);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ListaMeses);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.btnCargarDatos);
+            this.Controls.Add(this.lblArchivoCargar);
+            this.Controls.Add(this.btnSeleccionarArchivo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataClientes);
+            this.Controls.Add(this.lblResultadoConsulta);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.txtBuscarDocumento);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBuscarDocumento;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Label lblResultadoConsulta;
+        private System.Windows.Forms.DataGridView dataClientes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSeleccionarArchivo;
+        private System.Windows.Forms.Label lblArchivoCargar;
+        private System.Windows.Forms.Button btnCargarDatos;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.ListBox ListaMeses;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CargarData
+{
+    public partial class Form1 : Form
+    {
+        string[] contenidoArchivo;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            string Documento = txtBuscarDocumento.Text;
+            //string[] documentosList = Documento.Split(';');
+            //lblResultadoConsulta.Text = documentosList.ToString();
+
+            dataClientes.DataSource = BuscarDocumento(Documento);
+        }
+
+        private void btnSeleccionarArchivo_Click_1(object sender, EventArgs e)
+        {
+            lblEstado.Text = "Leyendo archivo....";
+            lblEstado.Visible = true;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "C:\\Users\\User\\Descargas";    // Directorio inicial
+            openFileDialog1.Filter = "Archivos de texto (*.txt)|*.txt|Todos los archivos (*.*)|*.*";  // Filtro de archivos
+            openFileDialog1.FilterIndex = 1;  // Primera opción seleccionada
+            openFileDialog1.RestoreDirectory = true;
+            string archivoSeleccionado = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                archivoSeleccionado = openFileDialog1.FileName;
+            }
+            lblArchivoCargar.Text = "Archivo: " + archivoSeleccionado.Replace("C:\\Users\\User\\Downloads\\", "");
+
+            contenidoArchivo = System.IO.File.ReadAllLines(archivoSeleccionado);
+            btnCargarDatos.Visible = true;
+        }
+
+        private void btnCargarDatos_Click(object sender, EventArgs e)
+        {
+            if (ListaMeses.SelectedItems.Count > 0)
+            {
+                string Mes = ListaMeses.SelectedItem.ToString();
+                DialogResult result = MessageBox.Show("se eliminara la información del MES: "+Mes+", desea continuar?", "Confirmación de eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    lblEstado.Text = "Inicia eliminado de informacion....";
+                    lblEstado.Refresh();
+                    Application.DoEvents();
+                    EliminaDataMes(Mes);
+                    lblEstado.Text = "Se elimina informacion, Empieza procesado del archivo....";
+                    lblEstado.Refresh();
+                    Application.DoEvents();
+                    List<string> ListaDatos = contenidoArchivo.ToList();
+                    ListaDatos.RemoveAt(0);
+                    contenidoArchivo = ListaDatos.ToArray();
+                    int CantidadTotal = contenidoArchivo.Count();
+                    DataTable tablaCargar = new DataTable();
+                    tablaCargar.Columns.Add("TIPO_DOC", typeof(string));
+                    tablaCargar.Columns.Add("NUM_DOC", typeof(string));
+                    tablaCargar.Columns.Add("P_APELLIDO", typeof(string));
+                    tablaCargar.Columns.Add("S_APELLIDO", typeof(string));
+                    tablaCargar.Columns.Add("P_NOMBRE", typeof(string));
+                    tablaCargar.Columns.Add("S_NOMBRE", typeof(string));
+                    tablaCargar.Columns.Add("F_NAC", typeof(string));
+                    tablaCargar.Columns.Add("SEXO", typeof(string));
+                    tablaCargar.Columns.Add("TIPO_AFILIADO", typeof(string));
+                    tablaCargar.Columns.Add("ESTADO", typeof(string));
+                    tablaCargar.Columns.Add("DEPARTAMENTO", typeof(string));
+                    tablaCargar.Columns.Add("MUNICIPIO", typeof(string));
+                    tablaCargar.Columns.Add("F_INICIO", typeof(string));
+                    tablaCargar.Columns.Add("DIRECCION", typeof(string));
+                    tablaCargar.Columns.Add("TELEFONO", typeof(string));
+                    tablaCargar.Columns.Add("CELULAR", typeof(string));
+                    tablaCargar.Columns.Add("CORREO", typeof(string));
+                    tablaCargar.Columns.Add("ZONA", typeof(string));
+                    tablaCargar.Columns.Add("PRESTADOR_MED", typeof(string));
+                    tablaCargar.Columns.Add("REGIMEN", typeof(string));
+                    int Conteo = 0;
+                    foreach (string linea in contenidoArchivo)
+                    {
+                        try
+                        {
+                            Conteo++;
+                            var columnas = linea.Split(';');
+                            DataRow Registro = tablaCargar.NewRow();
+                            Registro["TIPO_DOC"] = columnas[0].Replace("\"", "");
+                            Registro["NUM_DOC"] = columnas[1].Replace("\"", "");
+                            Registro["P_APELLIDO"] = columnas[2].Replace("\"", "");
+                            Registro["S_APELLIDO"] = columnas[3].Replace("\"", "");
+                            Registro["P_NOMBRE"] = columnas[4].Replace("\"", "");
+                            Registro["S_NOMBRE"] = columnas[5].Replace("\"", "");
+                            Registro["F_NAC"] = columnas[6].Replace("\"", "");
+                            Registro["SEXO"] = columnas[7].Replace("\"", "");
+                            Registro["TIPO_AFILIADO"] = columnas[8].Replace("\"", "");
+                            Registro["ESTADO"] = columnas[9].Replace("\"", "");
+                            Registro["DEPARTAMENTO"] = columnas[10].Replace("\"", "");
+                            Registro["MUNICIPIO"] = columnas[11].Replace("\"", "");
+                            Registro["F_INICIO"] = columnas[12].Replace("\"", "");
+                            Registro["DIRECCION"] = columnas[13].Replace("\"", "");
+                            Registro["TELEFONO"] = columnas[14].Replace("\"", "");
+                            Registro["CELULAR"] = columnas[15].Replace("\"", "");
+                            Registro["CORREO"] = columnas[16].Replace("\"", "");
+                            Registro["ZONA"] = columnas[17].Replace("\"", "");
+                            Registro["PRESTADOR_MED"] = columnas[18].Replace("\"", "");
+                            Registro["REGIMEN"] = columnas[19].Replace("\"", "");
+                            tablaCargar.Rows.Add(Registro);
+                            if (tablaCargar.Rows.Count == 100000 || Conteo == CantidadTotal)
+                            {
+                                InsertarData(tablaCargar, CantidadTotal, Conteo);
+                                tablaCargar.Clear();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            lblEstado.Text = "Se genero un error en la insercion registros procesados: " + Conteo + "........ Error: " + ex.Message;
+                            lblEstado.Refresh();
+                            Application.DoEvents();
+                        }
+                    }
+                    lblEstado.Text = "inicia actualizacion del mes ";
+                    lblEstado.Refresh();
+                    Application.DoEvents();
+                    ActualizarData(Mes);
+
+                }
+                else
+                {
+                    // El usuario seleccionó "No", cancelar la operación
+                    MessageBox.Show("La operación de eliminación ha sido cancelada.", "Operación Cancelada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleccione el mes a actualizar");
+            }
+            lblEstado.Text = "ESTADO: finaliza proceso archivo cargado exitosamente";
+            lblEstado.Refresh();
+            Application.DoEvents();
+        }
+
+        private void InsertarData(DataTable tabla, int cantidadTotal, int conteo)
+        {
+            string connectionString = "Data Source=.;Initial Catalog=Regimen;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlBulkCopy bulkCopy = new SqlBulkCopy(connection))
+                    {
+                        bulkCopy.DestinationTableName = "BaseEPS";
+                        bulkCopy.ColumnMappings.Add("TIPO_DOC", "TIPO_DOC");
+                        bulkCopy.ColumnMappings.Add("NUM_DOC", "NUM_DOC");
+                        bulkCopy.ColumnMappings.Add("P_APELLIDO", "P_APELLIDO");
+                        bulkCopy.ColumnMappings.Add("S_APELLIDO", "S_APELLIDO");
+                        bulkCopy.ColumnMappings.Add("P_NOMBRE", "P_NOMBRE");
+                        bulkCopy.ColumnMappings.Add("S_NOMBRE", "S_NOMBRE");
+                        bulkCopy.ColumnMappings.Add("F_NAC", "F_NAC");
+                        bulkCopy.ColumnMappings.Add("SEXO", "SEXO");
+                        bulkCopy.ColumnMappings.Add("TIPO_AFILIADO", "TIPO_AFILIADO");
+                        bulkCopy.ColumnMappings.Add("ESTADO", "ESTADO");
+                        bulkCopy.ColumnMappings.Add("DEPARTAMENTO", "DEPARTAMENTO");
+                        bulkCopy.ColumnMappings.Add("MUNICIPIO", "MUNICIPIO");
+                        bulkCopy.ColumnMappings.Add("F_INICIO", "F_INICIO");
+                        bulkCopy.ColumnMappings.Add("DIRECCION", "DIRECCION");
+                        bulkCopy.ColumnMappings.Add("TELEFONO", "TELEFONO");
+                        bulkCopy.ColumnMappings.Add("CELULAR", "CELULAR");
+                        bulkCopy.ColumnMappings.Add("CORREO", "CORREO");
+                        bulkCopy.ColumnMappings.Add("ZONA", "ZONA");
+                        bulkCopy.ColumnMappings.Add("PRESTADOR_MED", "PRESTADOR_MED");
+                        bulkCopy.ColumnMappings.Add("REGIMEN", "REGIMEN");
+                        bulkCopy.BulkCopyTimeout = 4200;
+                        bulkCopy.WriteToServer(tabla);
+                        lblEstado.Text = "se cargaron exitosamente " + conteo.ToString("N0")
+                            + " registros de " + cantidadTotal.ToString("N0");
+                        lblEstado.Refresh();
+                        Application.DoEvents();
+                    }
+
+                    Console.WriteLine("Inserción masiva completada exitosamente.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error durante la inserción masiva: " + ex.Message);
+                }
+            }
+        }
+
+        private void ActualizarData(string mes)
+        {
+            
+            string connectionString = "Data Source=.;Initial Catalog=Regimen;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlCommand cmd = new SqlCommand("EActualizaDataMes", connection))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@Mes", mes);
+                        cmd.CommandTimeout = 4200;
+                        cmd.ExecuteReader();
+                        lblEstado.Text = "finaliza actualizacion del mes ";
+                        lblEstado.Refresh();
+                        Application.DoEvents();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                    lblEstado.Text = "actualizacion del mes Error: " + ex.Message;
+                    lblEstado.Refresh();
+                    Application.DoEvents();
+                }
+            }
+        }
+
+        private void EliminaDataMes(string mes)
+        {
+            string connectionString = "Data Source=.;Initial Catalog=Regimen;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlCommand cmd = new SqlCommand("EliminarDataMes", connection))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@Mes", mes);
+                        cmd.ExecuteReader();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+
+        public DataTable BuscarDocumento(string documento) 
+        {
+            DataTable listaDevuelta = new DataTable();
+            string connectionString = "Data Source=.;Initial Catalog=Regimen;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlCommand cmd = new SqlCommand("BuscarDocumento", connection))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@Documento", documento);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            listaDevuelta.Load(reader);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+            return listaDevuelta;
+        }
+
+    }
+}
